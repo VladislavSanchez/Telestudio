e07ed6da7b95f1233fc753b7e19b456b2107ba27
Владислав С2 <VladislavKazakov2020@gmail.com>
2020-11-21 01:49:44 +0300

    Очередная доработка меню

--- JS/aboutUser.js
+++ JS/aboutUser.js
@@ -1,6 +1,9 @@
-var userMenu = document.querySelector('.menu__user');
-var userItem = document.querySelector('.menu__about-user');
+let userMenu = document.querySelector('.menu__user');
+let userItem = document.querySelector('.menu__about-user');
+let userName = document.querySelector('.menu__user-name');
 
 userMenu.addEventListener('click', function () {
     userItem.classList.toggle('menu__user_open');
+    userName.classList.toggle('menu__user-name-open');
+    userName.classList.toggle('menu__user-name');
 })
