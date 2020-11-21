6bf75f7321e0da5d4b640dc4ecaff1b66f4edf46
Владислав С2 <VladislavKazakov2020@gmail.com>
2020-11-21 15:06:45 +0300

    Очередная доработка меню

--- JS/aboutUser.js
+++ JS/aboutUser.js
@@ -1,9 +1,5 @@
-let userMenu = document.querySelector('.menu__user');
-let userItem = document.querySelector('.menu__about-user');
-let userName = document.querySelector('.menu__user-name');
-
-userMenu.addEventListener('click', function () {
-    userItem.classList.toggle('menu__user_open');
-    userName.classList.toggle('menu__user-name-open');
-    userName.classList.toggle('menu__user-name');
-})
+var app = new Vue {
+    openFunction(){
+        active: true;
+    }
+}
