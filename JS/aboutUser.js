c7479eb9cc2779892a9e9e91c4b3df1fd4c8ca5b
Владислав С2 <VladislavKazakov2020@gmail.com>
2020-11-29 02:02:25 +0300

    НЕМНОГО СПИСКА)))

--- JS/aboutUser.js
+++ JS/aboutUser.js
@@ -1,5 +1,9 @@
-var app = new Vue {
-    openFunction(){
-        active: true;
-    }
-}
+let userMenu = document.querySelector('.menu__user');
+let userItem = document.querySelector('.menu__about-user');
+let userName = document.querySelector('.menu__user-name');
+
+userMenu.addEventListener('click', function () {
+    userItem.classList.toggle('menu__user_open');
+    userName.classList.toggle('menu__user-name-open');
+    userName.classList.toggle('menu__user-name');
+})
