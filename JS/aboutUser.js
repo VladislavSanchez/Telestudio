a7dbc42432486ef1d82f14ab515bedb3bac9ed69
Владислав С2 <VladislavKazakov2020@gmail.com>
2021-01-07 14:27:39 +0300

    Добавлен footer and button "exit"

--- JS/aboutUser.js
+++ JS/aboutUser.js
@@ -1,9 +1,9 @@
 let userMenu = document.querySelector('.menu__user');
-let userItem = document.querySelector('.menu__about-user');
+let userItem = document.querySelector('.menu__about-user-progress');
 let userName = document.querySelector('.menu__user-name');
 
 userMenu.addEventListener('click', function () {
-    userItem.classList.toggle('menu__user_open');
-    userName.classList.toggle('menu__user-name-open');
-    userName.classList.toggle('menu__user-name');
+    userItem.classList.toggle('menu__about-user-progress--open');
+    userName.classList.toggle('menu__user-name-marker--close');
+    userName.classList.toggle('menu__user-name-marker--open');
 });
